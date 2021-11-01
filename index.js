667bc67aa4bed988fd7767dba1b1160b67876727
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-11-01 17:18:52 -0500

    commit tree e0b919645599d63226736e40d4b83a646824dab7

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ft){
+  if (ft <= 400) {
+    return 'This one is on me!';
+  } else if (ft > 2500) {
+    return 'No can do.';
+  } else if (ft > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch(tipAmount) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
